be0c480e982c47db38d012fef63a637b0a747d36
rliu <984544334@qq.com>
2024-07-08 18:42:41 +0800

    feat update web application config

--- backend/HermesWeb/src/main/java/com/hermes/HermesWebApplication.java
+++ backend/HermesWeb/src/main/java/com/hermes/HermesWebApplication.java
@@ -1,10 +1,14 @@
 package com.hermes;
 
+
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@EnableScheduling
 @Slf4j
 public class HermesWebApplication {
     public static void main(String[] args) {
